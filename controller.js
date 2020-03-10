eeb395d15f60a108bb05136b65a39f20e1cb2168
Dwi Prasetya Armunanta <armunantadwiprasetya@gmail.com>
2020-03-10 22:54:27 +0700

    crud server
    crud server

--- controller.js
+++ controller.js
@@ -1,13 +1,19 @@
 var mssql = require('mssql');
 var dbConfig = require('./config');
 
-module.exports.execQuery = function(req, res){
+module.exports.execQuery = function(req, res, param){
     var exec = mssql.connect(dbConfig, function(err){
         if(err){
             res.end("Connection error " + err);
         }
         else{
             var request = new mssql.Request();
+            // Ketika ada input parameter
+            if(param != null){
+                param.forEach(function(i){
+                    request.input(i.name, i.sqlType, i.value);
+                });
+            }
             request.query(req, function(err, response){
                 if(err){
                     console.log("Query error");
--- server.js
+++ server.js
@@ -1,17 +1,64 @@
 var express = require('express');
 var execute = require('./controller');
+var bodyParser = require('body-parser');
+var sql = require('mssql');
 
-var app = express();
-// app.use(app.router);
 var port = '8006';
+var cors = require("cors");
+var app = express();
+app.use(bodyParser.json());
+
+app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
+    next();
+});
 
 app.get('/', function(request, response){
    response.send("Halo-halo");
 });
 
+//Tampilkan semua data
 app.get('/api/mahasiswa', function(request, response){
-   var query = "select * from mahasiswa";
-   execute.execQuery(query, response);
+   var query = "SELECT * FROM jexcel";
+   execute.execQuery(query, response, null);
+});
+
+//Insert data
+app.post('/api/mahasiswa', function(request, response){
+   console.log("Query: Insert Data");
+   var query = "INSERT INTO jexcel(nrp, nama, angkatan, jenis_kelamin, tanggal_lahir, foto, aktif) VALUES('', '', '', '', '', '', '')";
+   execute.execQuery(query, response, null);
+});
+
+// Update data
+app.put('/api/mahasiswa/:id', function(request, response){
+   console.log("Query: Update Data");
+   // Parameter
+   var param = [
+      {name: 'id', sqlType: sql.Int, value: request.params.id},
+      {name: 'nrp', sqlType: sql.VarChar, value: request.body.nrp},
+      {name: 'nama', sqlType: sql.VarChar, value: request.body.nama},
+      {name: 'angkatan', sqlType: sql.Int, value: request.body.angkatan},
+      {name: 'jenis_kelamin', sqlType: sql.VarChar, value: request.body.jenis_kelamin},
+      {name: 'tanggal_lahir', sqlType: sql.Char, value: request.body.tanggal_lahir},
+      {name: 'foto', sqlType: sql.VarChar, value: request.body.foto},
+      {name: 'aktif', sqlType: sql.Bit, value: request.body.aktif}
+   ];
+   var query = "UPDATE jexcel SET nrp = @nrp, nama = @nama, angkatan = @angkatan, jenis_kelamin = @jenis_kelamin, tanggal_lahir = @tanggal_lahir, foto = @foto, aktif = @aktif WHERE id = @id";
+   execute.execQuery(query, response, param);
+});
+
+// Delete Data
+app.delete('/api/mahasiswa/:id', function(request, response){
+   console.log("Query: Delete Data");
+   // Parameter
+   var param = [{name: 'id', sqlType: sql.Int, value: request.params.id}];
+   var query = "DELETE FROM [jexcel] WHERE id=@id";
+   execute.execQuery(query, response, param);
+
 });
 
 app.listen(port, function(){
