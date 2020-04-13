fce7bf6bf82ddfaf63f99917a4f997db21558cf7
Dwi Prasetya Armunanta <armunantadwiprasetya@gmail.com>
2020-04-13 08:51:35 +0700

    crud 4 tabel

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const dbconfiguration = {
     server: '10.199.13.253', 
-    user: 'su',
+    user: 'sa',
     password: 'SaSa1212',
     database: 'nrp05111740000025',
     port: 1433
--- server.js
+++ server.js
@@ -26,6 +26,161 @@ app.get('/api/mahasiswa', function(request, response){
    execute.execQuery(query, response, null);
 });
 
+
+// CRUD Kategori UNIT
+
+// Tampilkan data kategori unit
+app.get('/api/kategori-unit', function(request, response){
+   var query = "SELECT * FROM KategoriUnit";
+   execute.execQuery(query, response, null);
+});
+
+// Insert Kategori Unit data
+app.post('/api/kategori-unit', function(request, response){
+   console.log("Query: Insert Kategori Unit Data");
+   var query = "INSERT INTO KategoriUnit(id, nama) VALUES('', '')";
+   execute.execQuery(query, response, null);
+});
+
+// Update Kategori Unit data
+app.put('/api/kategori-unit/:id', function(request, response){
+   console.log("Query: Update Kategori Unit Data");
+
+   var param = [
+      {name: 'id', sqlType: sql.Int, value:request.params.id},
+      {name: 'nama', sqlType: sql.VarChar, value:request.body.nama}
+   ];
+
+   var query = "UPDATE KategoriUnit SET nama = @nama WHERE id = @id";
+   execute.execQuery(query, response, param);
+});
+
+// Delete Kategori Unit Data
+app.delete('/api/kategori-unit/:id', function(request, response){
+   console.log("Query: Delete Kategori Unit Data");
+
+   var param = [{name: 'id', sqlType: sql.Int, value: request.params.id}];
+   var query = "DELETE FROM KategoriUnit WHERE id = @id";
+   execute.execQuery(query, response, param);
+});
+
+
+// CRUD Unit
+// Tampilkan data Unit
+app.get('/api/unit', function(request, response){
+   var query = "SELECT * FROM Unit";
+   execute.execQuery(query, response, null);
+});
+
+// Insert Unit data
+app.post('/api/unit', function(request, response){
+   console.log("Query: Insert Unit Data");
+   var query = "INSERT INTO Unit(id, KategoriUnit_id, nama) VALUES('', '', '')";
+   execute.execQuery(query, response, null);
+});
+
+// Update Unit data
+app.put('/api/unit/:id', function(request, response){
+   console.log("Query: Update Unit Data");
+   
+   var param = [
+      {name: 'id', sqlType: sql.Int, value: request.params.id},
+      {name: 'KategoriUnit_id', sqlType: sql.Int, value: request.body.KategoriUnit_id},
+      {name: 'nama', sqlType: sql.VarChar, value: request.body.nama}
+   ];
+
+   var query = "UPDATE Unit SET KategoriUnit_id = @KategoriUnit_id, nama = @nama WHERE id = @id";
+   execute.execQuery(query, response, param);
+});
+
+// Delete Unit Data
+app.delete('/api/unit/:id', function(request, response){
+   console.log("Query: Delete Unit Data");
+
+   var param = [{name: 'id', sqlType: sql.Int, value: request.params.id}];
+   var query = "DELETE FROM Unit WHERE id = @id";
+   execute.execQuery(query, response, param);
+});
+
+
+// CRUD Data Dasar
+// Tampilkan data data dasar
+app.get('/api/data-dasar', function(request, response){
+   var query = "SELECT * FROM DataDasar";
+   execute.execQuery(query, response, null);
+});
+
+// Insert Data Dasar data
+app.post('/api/data-dasar', function(request, response){
+   console.log("Query: Insert Data Dasar Data");
+   var query = "INSERT INTO DataDasar(id, nama) VALUES('', '')";
+   execute.execQuery(query, response, null);
+});
+
+// Update Data Dasar data
+app.put('/api/data-dasar/:id', function(request, response){
+   console.log("Query: Update Data Dasar Data");
+
+   var param = [
+      {name: 'id', sqlType: sql.Int, value: request.params.id},
+      {name: 'nama', sqlType: sql.VarChar, value: request.body.nama}
+   ];
+
+   var query = "UPDATE DataDasar SET nama = @nama WHERE id = @id";
+   execute.execQuery(query, response, param);
+});
+
+// Delete Data Dasar Data
+app.delete('/api/data-dasar/:id', function(request, response){
+   console.log("Query: Delete Data Dasar Data");
+
+   var param = [{name: 'id', sqlType: sql.Int, value: request.params.id}];
+   var query = "DELETE FROM DataDasar WHERE id = @id";
+   execute.execQuery(query, response, param);
+});
+
+
+// CRUD Capaian Unit
+// Tampilkan data capaian unit
+app.get('/api/capaian-unit', function(request, response){
+   var query = "SELECT * FROM Capaian_Unit";
+   execute.execQuery(query, response, null);
+});
+
+// Insert Capaian Unit data
+app.post('/api/capaian-unit', function(request, response){
+   console.log("Query: Insert Capaian_Unit Data");
+   var query = "INSERT INTO Capaian_Unit(id, DataDasar_id, Unit_id, waktu, capaian) VALUES('', '', '', '', '')";
+   execute.execQuery(query, response, null);
+});
+
+//Update Capaian_Unit data
+app.put('/api/capaian-unit', function(request, response){
+   console.log("Query: Update Capaian_Unit Data");
+
+   var param = [
+      {name: 'id', sqlType: sql.Int, value: request.params.id},
+      {name: 'DataDasar_id', sqlType: sql.Int, value: request.body.DataDasar_id},
+      {name: 'Unit_id', sqlType: sql.Int, value: request.body.Unit_id},
+      {name: 'waktu', sqlType: sql.DateTime, value: request.body.waktu},
+      {name: 'capaian', sqlType: sql.Float, value: request.body.capaian}
+   ];
+
+   var query = "UPDATE Capaian_Unit SET DataDasar_id = @DataDasar_id, Unit_id = @Unit_id, waktu = @waktu, capaian = @capaian WHERE id = @id";
+   execute.execQuery(query, response, param);
+});
+
+// Delete Capaian_Unit Data
+app.delete('/api/capaian-unit/:id', function(request, response){
+   console.log("Query: Delete Capaian_Unit Data");
+
+   var param = [{name: 'id', sqlType: sql.Int, value: request.params.id}];
+   var query = "DELETE FROM Capaian_Unit WHERE id = @id";
+   execute.execQuery(query, response, param);
+});
+
+
+
 //Insert data
 app.post('/api/mahasiswa', function(request, response){
    console.log("Query: Insert Data");
